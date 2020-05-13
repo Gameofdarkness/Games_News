e8a76d0f608e3edb2bff82e2bd7f53d1b8faede7
diyar-2020 <dyartymwry1@gmail.com>
2020-05-13 15:15:51 +0430

    45

--- offline.js
+++ offline.js
@@ -1,11 +1,13 @@
 ﻿{
-	"version": 1589363494,
+	"version": 1589366738,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"bg_red.png",
-		"scrool_bg.png"
+		"scrool_bg.png",
+		"sprite-sheet0.png",
+		"sprite2-sheet0.png"
 	]
 }
